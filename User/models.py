d3770cd3db538cd692480d700df229df4eff5721
Yididya <jedidiahandualem@gmail.com>
2023-06-15 01:02:14 +0300

    feat: create User model
    created a models that inherites from Abstract user

--- User/models.py
+++ User/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class User(AbstractUser):
+
+    class Meta:
+        swappable = 'AUTH_USER_MODEL'
+
+    def __str__(self):
+        return f'{self.username}-{self.first_name}'
