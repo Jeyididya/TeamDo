ae90ca00e2e51f49af773f6bdbbc907d170b8673
Yididya <jedidiahandualem@gmail.com>
2023-06-15 23:57:47 +0300

    feat: create view for notifaction list, detail and new notification views

--- Notification/views.py
+++ Notification/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Notification
+from .serializers import NotificationSerializer
+from django.db.models import Q
 
-# Create your views here.
+
+class NotificationListCreateView(generics.ListCreateAPIView):
+    serializer_class = NotificationSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Notification.objects.filter(user=user)
+
+
+class NewNotificationListCreateView(generics.ListCreateAPIView):
+    serializer_class = NotificationSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Notification.objects.filter(Q(user=user) & Q(is_read=False))
+
+
+class NotificationDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = NotificationSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Notification.objects.filter(user=user)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.is_read = True
+        instance.save()
+        return super().retrieve(request, *args, **kwargs)
